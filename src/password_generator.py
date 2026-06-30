586c73b1fc8a9ebeb9b073421df6690c7282a64a
Alik <alikhatsykov@gmail.com>
2026-06-30 12:51:56 +0300

    main

---
+++ src/password_generator.py
@@ -0,0 +1,15 @@
+import random
+import string
+
+def generator_password(lenght: int = 12, use_digits: bool = True, use_specials: bool = True) -> str:
+    chars = string.ascii_letters
+    if use_digits:
+        chars += string.digits
+    if use_specials:
+        chars += "!@#$%^&*"
+    
+
+    if not chars:
+        return "Mistake: change 1 symbol"
+    
+    return ''.join(random.choice(chars) for _ in range(lenght))
